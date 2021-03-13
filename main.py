a0988436f527ba8d0728e6b00849027b7a343a72
Shawn-Chahal <shawn.chahal@gmail.com>
2021-03-13 16:45:25 -0500

    Added separate output option

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ output_dir = "textures_output"
 
 
 def generate_mask_map(prefix, metallic=None, occlusion=None, detail=None, smoothness=None,
-                      smoothness_from_metallic_alpha=True):
+                      smoothness_from_metallic_alpha=True, output_file_format="png"):
     """
     • Red: Stores the metallic map.
     • Green: Stores the ambient occlusion map.
@@ -20,7 +20,7 @@ def generate_mask_map(prefix, metallic=None, occlusion=None, detail=None, smooth
     img_zero = None
 
     if metallic is not None:
-        img_metallic = Image.open(os.path.join(input_dir, f"{prefix}{metallic}.{file_format}"))
+        img_metallic = Image.open(os.path.join(input_dir, f"{prefix}{metallic}.{input_file_format}"))
         if smoothness_from_metallic_alpha:
             img["R"] = Image.fromarray(np.array(img_metallic.convert("RGBA"))[:, :, 0])
         else:
@@ -30,19 +30,19 @@ def generate_mask_map(prefix, metallic=None, occlusion=None, detail=None, smooth
         skipped.append("R")
 
     if occlusion is not None:
-        img["G"] = Image.open(os.path.join(input_dir, f"{prefix}{occlusion}.{file_format}")).convert("L")
+        img["G"] = Image.open(os.path.join(input_dir, f"{prefix}{occlusion}.{input_file_format}")).convert("L")
         img_zero = Image.fromarray(np.zeros_like(np.array(img["G"]))).convert("L")
     else:
         skipped.append("G")
 
     if detail is not None:
-        img["B"] = Image.open(os.path.join(input_dir, f"{prefix}{detail}.{file_format}")).convert("L")
+        img["B"] = Image.open(os.path.join(input_dir, f"{prefix}{detail}.{input_file_format}")).convert("L")
         img_zero = Image.fromarray(np.zeros_like(np.array(img["B"]))).convert("L")
     else:
         skipped.append("B")
 
     if smoothness is not None:
-        img_smoothness = Image.open(os.path.join(input_dir, f"{prefix}{smoothness}.{file_format}"))
+        img_smoothness = Image.open(os.path.join(input_dir, f"{prefix}{smoothness}.{input_file_format}"))
         if smoothness_from_metallic_alpha:
             img["A"] = Image.fromarray(np.array(img_smoothness.convert("RGBA"))[:, :, -1])
         else:
@@ -69,13 +69,13 @@ def generate_mask_map(prefix, metallic=None, occlusion=None, detail=None, smooth
             print(f"Resized {prefix} channel {channel} from {img_dim} to {max_size}.")
 
     rgba = Image.merge("RGBA", (img["R"], img["G"], img["B"], img["A"]))
-    rgba.save(os.path.join(output_dir, f"{prefix}mask_map.{file_format}"))
+    rgba.save(os.path.join(output_dir, f"{prefix}mask_map.{output_file_format}"))
 
     print(f"Created {prefix} mask map.")
 
 
-file_format = "png"
+input_file_format = "psd"
 
-generate_mask_map("basecolor_chandelier_4_", metallic="metallic", occlusion="occlusion", smoothness="metallic")
-generate_mask_map("glass_chandelier_4_", metallic="metallic", occlusion="occlusion", smoothness="metallic")
-generate_mask_map("lightbulb_chandelier_4_", metallic="metallic", occlusion="occlusion", smoothness="metallic")
+generate_mask_map("Table_6_", metallic="MetallRough", occlusion="AO", smoothness="MetallRough")
+generate_mask_map("Tables_2_", metallic="MetallRough", occlusion="AO", smoothness="MetallRough")
+generate_mask_map("Tables_", metallic="MetallRough", occlusion=None, smoothness="MetallRough")
